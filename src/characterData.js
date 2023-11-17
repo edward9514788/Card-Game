7a2de4221e6cd1ac34ca6496166fec9775483cd1
edward9514788 <edwardmodicue@gmail.com>
2023-11-17 16:29:45 -0600

    Added Character Data file "characterData.js"

---
+++ src/characterData.js
@@ -0,0 +1,28 @@
+
+[
+    {
+        name: "Goku",
+        health: 100,
+        attacks: [
+            {
+                attackName: "Kamehameha",
+                attackDescription: "A powerful blue energy wave",
+                attackImage: "Kamehameha.jpg"
+
+            },
+            {
+                attackName: "Spirit Bomb",
+                attackDescription: "A powerful large blue energy bomb",
+                attackImage: "SpiritBomb.jpg"
+
+            },
+            {
+                attackName: "Kaioken",
+                attackDescription: "A powerful dashing red combo",
+                attackImage: "Kaioken.jpg"
+
+            }
+        ],
+        characterImage: "Goku.jpg"
+    }
+]
