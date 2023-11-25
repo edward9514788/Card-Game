840aaa408a0323da73f30198364c0c79930a8cb9
edward9514788 <edwardmodicue@gmail.com>
2023-11-25 14:56:03 -0600

    Added "Disabled" feature based on whose turn it is

--- src/BodyComponent/BodyComponent.js
+++ src/BodyComponent/BodyComponent.js
@@ -1,18 +1,25 @@
 import PlayAreaComponent from "./PlayAreaComponent/PlayAreaComponent";
 import PlayerComponent from "./PlayerComponent/PlayerComponent";
 import characterData from "../characterData";
+import { useState } from "react";
 
 function BodyComponent() {
 
-    //let characterData
+    const [isDisabled, setIsDisabledState] = useState(false)
+
+    function onAttackClickHandler() {
+
+        setIsDisabledState( prevDisabledState => !prevDisabledState)
+
+    }
 
     return (
     
         <ul className="bodyComponent">
 
-            <li className="listItem1"><PlayerComponent player= {characterData[0]}></PlayerComponent> </li>
+            <li className="listItem1"><PlayerComponent player= {characterData[0]} isDisabled= {isDisabled} attackHandler={onAttackClickHandler}></PlayerComponent> </li>
             <li className="listItem2"><PlayAreaComponent></PlayAreaComponent></li>
-            <li className="listItem3"><PlayerComponent player= {characterData[1]}></PlayerComponent></li>
+            <li className="listItem3"><PlayerComponent player= {characterData[1]} isDisabled= {!isDisabled} attackHandler={onAttackClickHandler}></PlayerComponent></li>
 
         </ul>
 
--- src/BodyComponent/PlayerComponent/PlayerComponent.js
+++ src/BodyComponent/PlayerComponent/PlayerComponent.js
@@ -7,13 +7,13 @@ function PlayerComponent(props) {
 
             <img src= {props.player.characterImage}></img> <br></br>
 
-            <select name="Attack" id="Attack">
+            <select disabled= {props.isDisabled} name="Attack" id="Attack">
                 <option value="Attack 1">{props.player.attacks[0].attackName}</option>
                 <option value="Attack 2">{props.player.attacks[1].attackName}</option>
                 <option value="Attack 3">{props.player.attacks[2].attackName}</option>
             </select>
 
-            <select name="Defend" id="Defend">
+            <select disabled= {props.isDisabled} name="Defend" id="Defend">
                 <option value="Defend 1">{props.player.defends[0].defendName}</option>
                 <option value="Defend 2">{props.player.defends[1].defendName}</option>
                 <option value="Defend 3">{props.player.defends[2].defendName}</option>
@@ -21,7 +21,7 @@ function PlayerComponent(props) {
 
             <br/> <br/>
 
-            <button>Play</button>
+            <button onClick= {props.attackHandler} disabled= {props.isDisabled}>Play</button>
 
 
 
