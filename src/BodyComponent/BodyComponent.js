deb0b394ce502c49ace3bd572021d647119e9b5a
edward9514788 <edwardmodicue@gmail.com>
2023-11-25 16:40:50 -0600

    onAttackClickHandler resets attack/defend messages

--- src/BodyComponent/BodyComponent.js
+++ src/BodyComponent/BodyComponent.js
@@ -14,6 +14,9 @@ function BodyComponent() {
 
         setIsDisabledState( prevDisabledState => !prevDisabledState)
 
+        setAttackMessage("")
+        setDefendMessage("")
+
     }
 
     function liftUpDataHandler(e) {
